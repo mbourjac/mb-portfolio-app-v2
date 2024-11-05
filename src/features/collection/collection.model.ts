4995e4e593f261b7d829540e9426ac771484853b
Michaël Bourjac <michael.bourjac@gmail.com>
2024-11-05 08:30:57 +0100

    chore: remove hashes from build asset filenames

--- src/features/collection/collection.model.ts
+++ src/features/collection/collection.model.ts
@@ -4,7 +4,7 @@ export class Collection {
   constructor(private readonly data: CollectionData) {}
 
   get thumbnails() {
-    return Object.keys(this.data.thumbnails);
+    return this.sortPathsByIndex(this.data.thumbnails);
   }
 
   get previews() {
